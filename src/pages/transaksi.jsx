9d2d6e477d6363c800af3ca0cd110105d3d8bcc0
Dino <aldinorahman36@gmail.com>
2020-02-07 11:32:37 +0700

    dasd

--- src/pages/transaksi.jsx
+++ src/pages/transaksi.jsx
@@ -1,6 +1,7 @@
-import React,{useState,useEffect} from 'react';
+import React,{useState,useEffect,useRef} from 'react';
 import { Table,CustomInput } from 'reactstrap';
-
+import Axios from 'axios'
+import { ApiUrl, ApiUrlimage } from '../support/apiurl';
 
 
 
@@ -9,7 +10,54 @@ const Transaksi=()=>{
         addImageFileName:'pilih foto..',
         addImageFile:undefined,
     })
-    // useEffect(())
+    const [data,setdata]=useState(null)
+    const [datainput]=useState({
+        userid:useRef()
+    })
+    useEffect(()=>{
+        const fetchdata= async()=>{
+            try {
+                const fahkran=await Axios.get(`${ApiUrl}product/gettrans`)
+                // datainput.userid.current.focus()
+                setdata(fahkran.data)
+            } catch (error) {
+                console.log(error)
+            }
+        }
+        fetchdata()
+    },[])
+
+    const adddata=()=>{
+        var formdata=new FormData()
+        console.log(datainput.userid.current.value)
+        const {userid}=datainput
+        const data={
+          userid:userid.current.value,
+        }
+        // const token=localStorage.getItem('token')
+        var Headers={
+          headers:
+          {
+              'Content-Type':'multipart/form-data',
+            //   'Authorization':`Bearer ${token}`
+          },
+          
+        }
+        formdata.append('image',addimagefile.addImageFile)
+        formdata.append('data',JSON.stringify(data))
+        Axios.post(`${ApiUrl}product/posttrans`,formdata,{
+            headers:{
+                'Content-Type':'multipart/form-data',
+                //   'Authorization':`Bearer ${token}`
+            },
+        })
+        .then((res)=>{
+            setdata(res.data)
+        }).catch((err)=>{
+          console.log(err)
+        })
+      }
+
 
 
     const onAddImageFileChange=(event)=>{
@@ -22,6 +70,34 @@ const Transaksi=()=>{
             setimageadd({...addimagefile,addImageFileName:'pilih foto',addImageFile:undefined})
         }
     }
+    const renderdata=()=>{
+        if(data===null){
+            return (
+                <tr>
+                    <td>loading...</td>
+                </tr>
+            )
+        }else if(data.length===0){
+            return(
+                <tr>
+                    <td>tidak ada data...</td>
+                </tr>
+            )
+        }else{
+            return data.map((val,index)=>{
+                return (
+                    <tr key={index}>
+                        <td>{index+1}</td>
+                        <td>{val.userid}</td>
+                        <td>{val.status}</td>
+                        <td>{val.tanggal}</td>
+                        <td><img src={`${ApiUrlimage+val.paymentimg}`} alt={val.image} height='100px'/></td>
+                    </tr>
+                )
+            })
+        }
+    }
+
 
     return(
         <div>
@@ -36,14 +112,14 @@ const Transaksi=()=>{
                     </tr>
                 </thead>
                 <tbody>
-
+                    {renderdata()}
                 </tbody>
                 <tfoot>
                     <tr>
-                        <td><input type='number' placeholder='userid' /></td>
-                        <td><CustomInput type='file' label={addimagefile.addImageFileName} onChange={onAddImageFileChange}/></td>
-                        <td><button className='btn btn-primary'> add foto</button></td>
                         <td></td>
+                        <td><input type='number' placeholder='userid' ref={datainput.userid} /></td>
+                        <td><CustomInput id='foto' type='file' label={addimagefile.addImageFileName} onChange={onAddImageFileChange}/></td>
+                        <td><button onClick={adddata} className='btn btn-primary'> add foto</button></td>
                         <td></td>
                     </tr>
                 </tfoot>
---
+++ src/support/apiurl.js
@@ -0,0 +1,2 @@
+export const ApiUrl=`http://localhost:2020/`
+export const ApiUrlimage=`http://localhost:2020`
